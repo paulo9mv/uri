7e080114ba92a4d293e400af76d9104678c84d29
Paulo Viana <paulo9mv@yahoo.com.br>
2019-07-09 07:12:49 -0500

    added 1009.c

---
+++ 1009.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    char nome;
+    double salario,vendas,x;
+ 
+    scanf("%s %lf %lf", &nome, &salario, &vendas);
+ 
+    x = salario + (15*vendas)/100;
+ 
+    printf("TOTAL = R$ %.2f\n",x);
+ 
+    return 0;
+}
